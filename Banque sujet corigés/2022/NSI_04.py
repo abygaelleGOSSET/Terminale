b5cfa3d265065a2566b186a8f62e2fefae160937
abygaelleGOSSET <70756300+abygaelleGOSSET@users.noreply.github.com>
2022-02-21 12:12:40 +0100

    Update NSI_04.py

--- Banque sujet corigés/2022/NSI_04.py
+++ Banque sujet corigés/2022/NSI_04.py
@@ -15,19 +15,19 @@ def propager(M,i,j,val):
     
     #L'élément en haut fait partie de la composante
     if((i-1)>=0 and M[i-1][j]==1):
-        return propager(M,i-1,j,val)
+        propager(M,i-1,j,val)
         
     #L'élément en bas fait partie de la composante
     if((i+1)<len(M) and M[i+1][j]==1):
-        return propager(M,i+1,j,val)
+        propager(M,i+1,j,val)
         
     #L'élément à gauche fait partie de la composante
     if((j-1)>=0 and M[i][j-1]==1):
-        return propager(M,i,j-1,val)
+        propager(M,i,j-1,val)
     
     #L'élément à droite fait partie de la composante
     if((j+1)<len(M) and M[i][j+1]==1):
-        return propager(M,i,j+1,val)
+        propager(M,i,j+1,val)
         
 M=[[0,0,1,0],[0,1,0,1],[1,1,1,0],[0,1,1,0]]
 propager(M,2,1,3)
